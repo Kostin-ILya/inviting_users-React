fbea074cfb95edfcf89b57395282763d2e44bf66
Ilya <lord-ilja@yandex.ru>
2022-12-28 15:41:53 +0300

    init commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import { Success } from './components/Success'
+import { Users } from './components/Users/'
+
+import './index.scss'
+
+// https://reqres.in/api/users
+
+function App() {
+  return (
+    <div className="App">
+      <Users />
+      {/* <Success /> */}
+    </div>
+  )
+}
+
+export default App
